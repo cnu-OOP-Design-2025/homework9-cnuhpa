5e0c5a060a450d86384ffaca9948c7a385d3b553
cnuhpa <rlghd332@o.cnu.ac.kr>
2025-11-18 14:25:43 +0900

    과제 제출

--- MyVector.cpp
+++ MyVector.cpp
@@ -6,11 +6,53 @@ using namespace std;
 template<typename T>
 class MyVector {
 /* TODO */
+private:
+    T* data;
+    size_t capacity;
+    size_t length;
 
+    void resize() {
+        cout << "Resize: " << capacity << " -> " << capacity * 2 << endl;
 
+        capacity = capacity * 2;
+        T* new_data = new T[capacity];
+        for (int i = 0; i < length; i++) {
+            new_data[i] = data[i];
+        }
+        delete[] data;
+        data = new_data;
+    }
 
+public:
 
+    MyVector() : capacity(2), length(0) {
+        data = new T[capacity];
+    }
 
+    ~MyVector() {
+        delete[] data;
+    }
+
+    void push_back(const T& value) {
+        if (length == capacity) resize();
+
+        data[length] = value;
+        length++;
+    }
+
+    T pop_back() {
+        if (length == 0)
+            throw out_of_range("Vector is empty");
+        return data[--length];
+    }
+
+    T& operator[](const int index){
+        return data[index];
+    }
+
+    size_t size() const {
+        return length;
+    }
 
 };
 
